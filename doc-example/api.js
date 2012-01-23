daff8d922dd3d3cfac18563db28275bc67d6f73c
unknown <nyffenegger@STRUPPI2.intelliact-net.local>
2012-01-23 10:08:20 +0100

    updated example

---
+++ doc-example/api.js
@@ -0,0 +1,3 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {"classes":["RequestHandler","SampleClass"],"modules":["RESTest"],"allModules":[{"displayName":"RESTest","name":"RESTest","description":"This is an example module which explains how RESTest works \ntogether with yuidoc. But it could be any other documentation Library"}]} };
+});
